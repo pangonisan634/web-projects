5a66dccaa269bb340bb7f81fb5fa68160f7f4155
pangonisan634 <258305501+pangonisan634@users.noreply.github.com>
2026-04-20 14:02:45 -0400

    Future War Archive is now seperate

---
+++ prompt-template.js
@@ -0,0 +1,10 @@
+// Creator
+// [REDACTED]
+// [TOP SECRET] Biography
+
+let username = prompt('Please enter the password.')
+let authentication = prompt('Input verification code.')
+
+let msg = `Disclaimer, what you about to see may be graphic, will you continue?`
+
+alert(msg);
