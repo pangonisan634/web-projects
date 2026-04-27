6928fe20ffde1ecfe59ca49aa861b94a9f0a6c73
pangonisan634 <258305501+pangonisan634@users.noreply.github.com>
2026-04-27 14:28:44 -0400

    made and all

--- javascript-gallery/functions-evidence/starter.js
+++ javascript-gallery/functions-evidence/starter.js
@@ -2,3 +2,37 @@
 // 14 APR 2026
 // Functions Evidence Test
 
+// Write or set up your three functions
+
+// Function to get BASE of triangle from user
+function getBase(){
+    const base = parseFloat(prompt("Enter the BASE of the right triangle:"));
+    return base;
+
+}
+
+
+
+
+// Function to get HEIGHT of triangle from user
+function getHeight(){
+    const height = parseFloat(prompt("Enter the HEIGHT of the right triangle:"));
+    return height;
+
+}
+
+
+
+
+// Do the math -- calculate the AREA of right triangle
+function calculateArea(base, height){
+    const area = (base * height) / 2
+    return area;
+}
+
+
+const myBase = getBase();
+const myHeight = getHeight();
+const myArea = calculateArea(myBase, myHeight);
+
+alert(`Base: ${myBase}  Height: ${myHeight}  Area: ${myArea}`);
